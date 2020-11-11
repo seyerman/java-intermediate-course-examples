2d0e4f3caac9cc22e7bcf7883aa1060800d355f4
Juan Manuel Reyes Garcia <seyerman@gmail.com>
2020-11-11 10:14:29 -0500

    Refactoring the UI in thread progess simple bar adding another class

--- threads_progress_bar/src/thread/SimpleProgressBarThread.java
+++ threads_progress_bar/src/thread/SimpleProgressBarThread.java
@@ -1,23 +1,23 @@
 package thread;
 
 import model.SimpleProgressBar;
-import ui.Main;
+import ui.SimpleProgressBarUI;
 
 public class SimpleProgressBarThread extends Thread{
 	private SimpleProgressBar spb;
-	private Main main;
+	private SimpleProgressBarUI spbui;
 	private int repetitions;
 	
-	public SimpleProgressBarThread(SimpleProgressBar s, Main m, int rep) {
+	public SimpleProgressBarThread(SimpleProgressBar s, SimpleProgressBarUI ui, int rep) {
 		spb = s;
-		main = m;
+		spbui = ui;
 		repetitions = rep;
 	}
 	
 	public void run() {
 		while(repetitions>0) {
 			spb.advance();
-			main.printProgressBar();
+			spbui.printProgressBar();
 			repetitions--;
 			try {
 				Thread.sleep(500);
